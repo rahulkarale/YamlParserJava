033f8f66f21abd8f4540db9c6496a5e300c0912f
rahulkarale <gangaram.karale@gmail.com>
2017-11-22 22:28:39 +0530

    sadghsad

---
+++ YamlParser/src/main/java/com/example/demo/YamlParserApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+
+public class YamlParserApplication {
+
+	public static void main(String[] args) {
+		
+		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        try {
+            Application user = mapper.readValue(new File("C:\\Users\\gangya\\Documents\\workspace-sts-3.9.0.RELEASE\\YamlParser\\src\\main\\resources\\user.yaml"), Application.class);
+            System.out.println(user.getName()+"\n"+user.getAddress());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	}
+}
